65c43eb44eb5c4dcfe32f78393fe74df7ea2e0f4
Manuel Figueiredo <manel.gf.95@gmail.com>
2025-09-18 13:11:12 +0200

    add action selector

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { ListPlus } from "@lucide/svelte";
+</script>
+
+<div class="border-b-2 border-gray-600 p-1 mb-4 flex justify-between">
+    <a href="/create" class="flex items-center gap-2 hover:bg-white/10 hover:scale-105 transition-transform duration-200 rounded-lg px-2 py-1">
+        <ListPlus size="18" />
+        Create Adventure
+    </a>
+</div>
---
+++ src/routes/create/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    const actors = ["Narrator", "Player"];
+
+    let addingAction = false;
+    let addNarratorAction = false;
+    let addPlayerAction = false;
+
+    let selectedActor = '';
+
+    function handleAdd(actor: string) {
+        addingAction = true;
+        if (actor === 'Narrator') addNarratorAction = true;
+        if (actor === 'Player') addPlayerAction = true;
+    }
+</script>
+
+<div class="flex flex-col w-full">
+    {#if addingAction}
+        <div>
+            {#if addNarratorAction}
+                <p>Narrator</p>
+            {/if}
+            {#if addPlayerAction}
+                <p>Player</p>
+            {/if}
+        </div>
+    {:else}
+        <div class="flex justify-between w-full">
+            {#each actors as a}
+                <button on:click={() => handleAdd(a)} class="bg-white/10 px-2 py-1 rounded-lg hover:bg-white/20 hover:scale-105 transition-transform duration-200">{`Add ${a} Action`}</button>
+            {/each}
+        </div>
+    {/if}
+</div>

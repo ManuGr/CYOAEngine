332439c99afcfb708688408bc937001a34075768
Manuel Figueiredo <manel.gf.95@gmail.com>
2025-09-18 11:30:59 +0200

    add navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,13 @@
+<script>
+    import logo from "$lib/assets/Manuel.jpeg"
+</script>
+
+<nav class="bg-[#36454f] fixed w-full z-20 start-0 border-b-3 border-gra-600">
+    <div class="max-w-screen-xl flex items-center justify-between mx-auto py-4">
+        <h1 class="text-xl">CYOA Engine</h1>
+        <a href="#" class="flex items-center gap-2">
+            <h2>Manuel Figueiredo</h2>
+            <img src={logo} alt="Profile pic" class="w-8 h-8 rounded-full">
+        </a>
+    </div>
+</nav>
